b05465afdc3e52e49e3fa735b5a362e36c18cf0a
Sarvar Ergashev <ergashev.sarvar@gmail.com>
2020-07-06 15:55:10 +0500

    final changes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -144,4 +144,27 @@ $(document).ready(function () {
     $(this).find('ul').slideToggle();
   });
 
+  $('.js-tab-trigger').click(function () {
+    var id = $(this).attr('data-tab'),
+      content = $('.js-tab-content[data-tab="' + id + '"]');
+
+    $('.js-tab-trigger.active').removeClass('active'); // 1
+    $(this).addClass('active'); // 2
+
+    $('.js-tab-content.active').removeClass('active'); // 3
+    content.addClass('active'); // 4
+  });
+
+  /* fancybox */
+  $('[data-fancybox="gallery"]').fancybox({
+    buttons: [
+      "zoom",
+      "share",
+      "fullScreen",
+      "download",
+      "close"
+    ],
+    infobar: false,
+  });
+
 });
